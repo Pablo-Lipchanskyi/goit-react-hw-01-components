98ccf4762a60acf6eddd33f8161e0ea4b1cfdc04
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-12-15 19:16:13 +0200

    profile css

--- src/components/Profile/userProfileComponent.js
+++ src/components/Profile/userProfileComponent.js
@@ -21,16 +21,16 @@ const Profile = ({
 
         <ul className={css.stats}>
     <li className={css.statsUnit}>
-        <span className="label">Followers</span>
-        <span className="quantity">{stats.followers}</span>
+            <span className={css.label}>Followers</span>
+            <span className={css.quantity}>{stats.followers}</span>
     </li>
     <li className={css.statsUnit}>
-        <span className="label">Views</span>
-        <span className="quantity">{stats.views}</span>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{stats.views}</span>
     </li>
     <li className={css.statsUnit}>
-        <span className="label">Likes</span>
-        <span className="quantity">{stats.likes}</span>
+        <span className={css.label}>Likes</span>
+            <span className={css.quantity}>{stats.likes}</span>
     </li>
   </ul>
 </div>
