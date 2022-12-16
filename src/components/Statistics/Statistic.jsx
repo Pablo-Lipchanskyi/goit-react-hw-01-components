63c99631261b6e03b030dfbc8dbd0ef509a729c3
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-12-16 16:33:08 +0200

    stats css

--- src/components/Statistics/Statistic.jsx
+++ src/components/Statistics/Statistic.jsx
@@ -5,13 +5,13 @@ export const Statistic = ({
 }) => {
     return (
         <section className={css.statistics}>
-        {title.length > 0 && (<h2 className="title">{title}</h2>)}
+        {title.length > 0 && (<h2 className={css.title}>{title}</h2>)}
 
-  <ul className="stat-list">
+  <ul className={css.statlist}>
     {stats.map(stat => (
-        <li key={stat.id} className="item">
-            <span className="label">{stat.label}</span>
-            <span className="percentage">{stat.percentage}</span>
+        <li key={stat.id} className={css.item}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}</span>
         </li>
         ))}
   </ul>
